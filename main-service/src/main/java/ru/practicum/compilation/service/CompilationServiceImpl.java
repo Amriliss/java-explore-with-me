41ebeae987211c850d76381837ec6e1744051c2c
Binom7 <amriliss@yandex.ru>
2023-09-15 12:58:08 +0300

    Доработка по замечаниям 1

--- main-service/src/main/java/ru/practicum/compilation/service/CompilationServiceImpl.java
+++ main-service/src/main/java/ru/practicum/compilation/service/CompilationServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.compilation.dto.CompilationDto;
 import ru.practicum.compilation.dto.NewCompilationDto;
 import ru.practicum.compilation.dto.UpdateCompilationRequest;
@@ -23,14 +22,14 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+
 public class CompilationServiceImpl implements CompilationService {
     private final CompilationRepository compilationRepository;
     private final EventRepository eventRepository;
     private final CompilationMapper compilationMapper;
 
     @Override
-    @Transactional
+
     public CompilationDto add(NewCompilationDto compilationDto) {
         Compilation compilation = compilationMapper.newCompilationDtoToCompilation(compilationDto);
 
@@ -45,7 +44,7 @@ public class CompilationServiceImpl implements CompilationService {
     }
 
     @Override
-    @Transactional
+
     public CompilationDto update(Long compId, UpdateCompilationRequest compRequest) {
         Compilation compilation = compilationRepository.findById(compId).orElseThrow(() -> {
             throw new ObjectNotFoundException("Compilation with id = " + compId + " doesn't exist.");
@@ -82,7 +81,7 @@ public class CompilationServiceImpl implements CompilationService {
     }
 
     @Override
-    @Transactional
+
     public void delete(Long compId) {
         try {
             compilationRepository.deleteById(compId);
@@ -91,7 +90,7 @@ public class CompilationServiceImpl implements CompilationService {
         }
     }
 
-    @Transactional
+
     public void updateComp(Compilation compilation, UpdateCompilationRequest compRequest) {
         if (compRequest.getEvents() != null) {
             List<Event> events = eventRepository.findAllByIdIn(compRequest.getEvents());
